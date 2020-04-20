fdf597ed631fe6b613e53f139c1ecb35e6d4c1f0
CDLarsen <curtis.larssen@gmail.com>
2020-04-20 15:57:28 +1300

    Settings Added

--- src/components/GmStats.js
+++ src/components/GmStats.js
@@ -1,16 +1,20 @@
 import React from 'react'
+import { IoMdSettings } from "react-icons/io";
 import { GmData } from '../data'
 import Card from './Card'
-import ThemeContext, { themes } from '../context/theme'
+import Settings from './Settings'
+import { themes } from '../context/theme'
 
 export default function GmStats() {
-    const [theme, setTheme] = React.useState(themes.detTheme)
-
+    const [theme, setTheme] = React.useState(themes.wpgTheme)
     return (
-            <div className='wrapper' style={theme}>
-                <div className="gm-grid">
-                    {Object.values(GmData).map(obj => <Card key={obj.id} {...obj.gmRanking} theme={theme} />)}
-                </ div>
+        <div className='wrapper' style={theme}>
+        <Settings/>
+            <IoMdSettings size={50} className='settings'/>
+            <div className="gm-grid">
+                {Object.values(GmData).map(obj => <Card key={obj.id} {...obj.gmRanking} theme={theme} />)}
             </div>
+            {console.log(themes)}
+        </div>
     )
 }
--- src/context/theme.js
+++ src/context/theme.js
@@ -2,156 +2,219 @@ import React from 'react'
 
 export const themes = {
     anaTheme: {
+        id: 'Anaheim Ducks',
+        val: 'themes.anaTheme',
         color: '#F47A38',
         borderColor: 'black',
         backgroundImage: 'url(https://i.imgur.com/9AtyZhr.jpg)'
     },
     ariTheme: {
+        id: 'Arizona Coyotes',
+        val: 'ari',
         color: '#8C2633',
         borderColor: '#E2D6B5',
         backgroundImage: 'url(https://i.imgur.com/SEcJnw8.jpg)'
     },
     bosTheme: {
+        id: 'Boston Bruins',
+        val: 'bos',
+        team: 'Anaheim',
         color: 'black',
         borderColor: '#FFB81C',
         backgroundImage: 'url(https://i.imgur.com/2wDTqfO.jpg)'
     },
     bufTheme: {
+        id: 'Buffalo Sabres',
+        val: 'buf',
         color: '#002654',
         borderColor: '#FCB514',
         backgroundImage: 'url(https://i.imgur.com/ZFvY1jU.jpg)'
     },
     calTheme: {
+        id: 'Calgary Flames',
+        val: 'cal',
         color: '#C8102E',
         borderColor: '#F1BE48',
         backgroundImage: 'url(https://i.imgur.com/gTGpFxT.jpg)'
     },
     carTheme: {
+        id: 'Carolina Hurricanes',
+        val: 'car',
         color: '#CC0000',
         borderColor: 'black',
         backgroundImage: 'url(https://i.imgur.com/jS95bjT.jpg)'
     },
     chiTheme: {
+        id: 'Chicago Blackhawks',
+        val: 'chi',
         color: '#CF0A2C',
         borderColor: 'black',
         backgroundImage: 'url(https://i.imgur.com/kY0Xw2I.jpg)'
     },
     colTheme: {
+        id: 'Colorado Avalanche',
+        val: 'col',
         color: '#6F263D',
         borderColor: '#236192',
         backgroundImage: 'url(https://i.imgur.com/frpcOPU.jpg)'
     },
     cmbTheme: {
+        id: 'Columbus Blue Jackets',
+        val: 'cmb',
         color: '#002654',
         borderColor: 'CE1126',
         backgroundImage: 'url(https://i.imgur.com/0NgAl0v.jpg)'
     },
     dalTheme: {
+        id: 'Dallas Stars',
+        val: 'dal',
         color: '#006847',
         borderColor: '#8F8F8C',
         backgroundImage: 'url(https://i.imgur.com/4xrMVFA.jpg)'
     },
     detTheme: {
+        id: 'Detroit Red Wings',
+        val: 'det',
         color: '#CE1126',
         borderColor: '#FFFFFF',
         backgroundImage: 'url(https://i.imgur.com/CmK04sz.jpg)'
     },
     edmTheme: {
+        id: 'Edmonton Oilers',
+        val: 'edm',
         color: '#041E42',
         borderColor: '#FF4C00',
         backgroundImage: 'url(https://i.imgur.com/5rI5hNz.jpg)'
     },
     floTheme: {
+        id: 'Florida Panthers',
+        val: 'flo',
         color: '#041E42',
         borderColor: '#C8102E',
         backgroundImage: 'url(https://i.imgur.com/MMy5txS.jpg)'
     },
     losTheme: {
+        id: 'Los Angeles Kings',
+        val: 'los',
         color: 'black',
         borderColor: '#A2AAAD',
         backgroundImage: 'url(https://i.imgur.com/ys007oK.jpg)'
     },
     minTheme: {
+        id: 'Minnesota Wild',
+        val: 'min',
         color: '#154734',
         borderColor: 'A6192E',
         backgroundImage: 'url(https://i.imgur.com/hqzG2WY.jpg)'
     },
     monTheme: {
+        id: 'Montreal Canadiens',
+        val: 'mon',
         color: '#AF1E2D',
         borderColor: '#192168',
         backgroundImage: 'url(https://i.imgur.com/zYaorpI.jpg)'
     },
     nshTheme: {
+        id: 'Nashville Predators',
+        val: 'nsh',
         color: '#FFB81C',
         borderColor: '#041E42',
         backgroundImage: 'url(https://i.imgur.com/6rocsS7.jpg)'
     },
     njTheme: {
+        id: 'New Jersey Devils',
+        val: 'nj',
         color: '#CE1126',
         borderColor: 'black',
         backgroundImage: 'url(https://i.imgur.com/JoJk8lo.jpg)'
     },
     nyiTheme: {
+        id: 'New York Islanders',
+        val: 'nyi',
         color: '#00539B',
         borderColor: '#F47D30',
         backgroundImage: 'url(https://i.imgur.com/z0CTxTc.jpg)'
     },
     nyrTheme: {
+        id: 'New York Rangers',
+        val: 'nyr',
         color: '#0038A8',
         borderColor: '#CE1126',
         backgroundImage: 'url(https://i.imgur.com/QmqHcOB.jpg)'
     },
     ottTheme: {
+        id: 'Ottawa Senators',
+        val: 'ott',
         color: '#C52032',
         borderColor: '#C2912C',
         backgroundImage: 'url(https://i.imgur.com/azcezUX.jpg)'
     },
     phiTheme: {
+        id: 'Philadelphia Flyers',
+        val: 'phi',
         color: '#F74902',
         borderColor: 'black',
         backgroundImage: 'url(https://i.imgur.com/xDiea1J.jpg)'
     },
     pitTheme: {
+        id: 'Pittsburgh Penguins',
+        val: 'pit',
         color: 'black',
         borderColor: '#CFC493',
         backgroundImage: 'url(https://i.imgur.com/2p9INer.jpg)'
     },
     sjTheme: {
+        id: 'San Jose Sharks',
+        val: 'sj',
         color: '#006D75',
         borderColor: '#EA7200',
         backgroundImage: 'url(https://i.imgur.com/bjgAujv.jpg)'
     },
     stlTheme: {
+        id: 'St. Louis Blues',
+        val: 'stl',
         color: '#002F87',
         borderColor: '#FCB514',
         backgroundImage: 'url(https://i.imgur.com/kJotXt9.jpg)'
     },
     tbTheme: {
+        id: 'Tampa Bay Lightning',
+        val: 'tbl',
         color: '#002868',
         borderColor: 'black',
         backgroundImage: 'url(https://i.imgur.com/UPIgpIG.jpg)'
     },
     torTheme: {
+        id: 'Toronto Maple Leafs',
+        val: 'tor',
         color: '#00205B',
         borderColor: 'white',
         backgroundImage: 'url(https://i.imgur.com/g697jXK.jpg)'
     },
     vanTheme: {
+        id: 'Vancouver Canucks',
+        val: 'van',
         color: '#00205B',
         borderColor: '#041C2C',
         backgroundImage: 'url(https://i.imgur.com/6GVdrkZ.jpg)'
     },
     vgsTheme: {
+        id: 'Las Vegas Knights',
+        val: 'vgs',
         color: '#B4975A',
         borderColor: '#333F42',
         backgroundImage: 'url(https://i.imgur.com/aLJ3rNa.jpg)'
     },
     wshTheme: {
+        id: 'Washington Capitals',
+        val: 'wsh',
         color: '#041E42',
         borderColor: '#C8102E',
         backgroundImage: 'url(https://i.imgur.com/OMZiAF0.jpg)'
     },
     wpgTheme: {
+        id: 'Winnipeg Jets',
+        val: 'wpg',
         color: '#041e42',
         borderColor: '#a2aaad',
         backgroundImage: 'url(https://i.imgur.com/Bpfq4Y8.jpg)'
