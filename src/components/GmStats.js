c6e3b3e659e7a12cbeee433d76d837b509cd0c13
CDLarsen <curtis.larssen@gmail.com>
2020-04-21 16:47:52 +1300

    SaveTheme added

--- src/components/GmStats.js
+++ src/components/GmStats.js
@@ -6,10 +6,10 @@ import { IoMdSettings, IoMdClose } from "react-icons/io";
 
 
 export default function GmStats() {
-    const [theme, setTheme] = React.useState(themes.wpgTheme)
+    const savedTheme = JSON.parse(localStorage.getItem('theme'))
+    const [theme, setTheme] = React.useState(savedTheme || themes.wpgTheme)
     const [slide, setSlide] = React.useState(false)
     const toggleSlide = () => setSlide((slider) => slider === true ? false : true)
-
 
     return (
         <div className='wrapper' style={theme}>
@@ -18,15 +18,24 @@ export default function GmStats() {
                 <IoMdClose size={50} onClick={toggleSlide} className='settings' color={theme.color} />
                 <div className='settings-wrapper'>
                     <ul className='theme-grid'>
-                        {Object.values(themes).map(obj => <li className='team-button' onClick={() => { setTheme(obj); toggleSlide()}} key={obj.id} style={{ backgroundColor: obj.color, borderColor: obj.borderColor }}>
-                        {obj.team}
-                        </li>)}
+                        {Object.values(themes).map(obj =>
+                            <li className='team-button'
+                                onClick={() => {
+                                    setTheme(obj);
+                                    toggleSlide();
+                                    localStorage.setItem('theme', JSON.stringify(obj))
+                                }}
+                                key={obj.id}
+                                style={{ backgroundColor: obj.color, borderColor: obj.borderColor }}>
+                                {obj.team}
+                            </li>)}
                     </ul>
                 </div>
             </div>
             <div className="gm-grid">
                 {Object.values(GmData).map(obj => <Card key={obj.id} {...obj.gmRanking} theme={theme} />)}
             </div>
+            {console.log(Object.values(localStorage))}
         </div>
     )
 }
