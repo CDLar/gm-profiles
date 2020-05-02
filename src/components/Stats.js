df21983b35084205d20d63a5081769e6a49c5c5a
CDLarsen <curtis.larssen@gmail.com>
2020-05-02 16:08:56 +1200

    1111

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,11 +1,38 @@
 import React from 'react'
+import { GmData } from '../data'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import Button from '@material-ui/core/Button';
+import Divider from '@material-ui/core/Divider';
 
-function Stats({theme}) {
+
+function Stats({ theme }) {
+    const [activeBtn, setActiveBtn] = React.useState('Buckley')
+
     return (
         <div className='stats-wrapper' style={{ backgroundColor: theme.color, borderColor: theme.borderColor }}>
-            Hello
+            <div className="team-nav">
+                {Object.values(GmData).map(obj =>
+                    <>
+                        <Button
+                            className='team-nav'
+                            style={{
+                                color: 'white', borderRadius: 0,
+                                borderLeft: obj.id === activeBtn ? `15px solid ${theme.borderColor}` : '',
+                                fontWeight: obj.id === activeBtn ? '700' : '400'
+                            }}
+                            onClick={() => setActiveBtn(obj.id)}
+                            key={obj.id}
+                            {...obj.gmRanking}
+                            theme={theme}
+                        >
+                            {obj.gmRanking.teamName}</Button>
+                        <Divider variant='middle' style={{ backgroundColor: theme.borderColor }} />
+                    </>
+                )}
+            </div>
         </div>
     )
 }
 
 export default Stats
+
